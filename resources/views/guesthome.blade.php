4be63f6e2d531931e46651200300883574ec3323
Suchu <sulochana.bhujel92@gmail.com>
2015-06-16 11:33:42 +0545

    Laravel project

---
+++ resources/views/guesthome.blade.php
@@ -0,0 +1,32 @@
+<html>
+<head><title>Guest Diary</title>
+	<link rel="stylesheet" type="text/css" href="{{url('css/style.css')}}">
+	<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"> -->
+
+</head>
+<body>
+	<div id = "wrapper">
+	<div id ="header"></div>
+	<div id = "container">
+		<div id ="content">
+			<h3>Suchu <span class="location">@Syangja</span><span class = "date" >2:21 PM 15 June,2015</span></h3>
+			<p>Hello, this is Suchu!</p>
+		</div>
+			<div id ="fill-form">
+			<form class="form1">
+				  <label>Name</label><br>
+				  <input type="text" class="span2" ><br>
+				  <label>Email</label><br>
+				  <input type="text" class="span2" ><br>
+				  <label>Location</label><br>
+				  <input type="text" class="span2" ><br>
+				  <label>Message</label><br>
+				  <textarea type="textarea" class="span2" cols="20" rows="10" ></textarea><br><br>
+				  <button type="submit" class="myButton"">Send</button>
+				  <!-- <a href="#" class="myButton">grey</a> -->
+				</div>
+		</div>
+	
+	</div>
+</body>
+</html>
